8dc8e8c8e7dc5333fb5dfd226745a146c8ae1e0e
Yang Guo <yangguoif@gmail.com>
2024-06-11 11:47:00 +1200

    configure telescope

--- macos/init.lua
+++ macos/init.lua
@@ -21,3 +21,30 @@ require'nvim-treesitter.configs'.setup {
 require("nvim-tree").setup({})
 require("nvim-autopairs").setup {}
 require('nvim-ts-autotag').setup({})
+
+-- Configuration for telescope
+-- Put file_name at the beginning of picker results.
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "TelescopeResults",
+  callback = function(ctx)
+    vim.api.nvim_buf_call(ctx.buf, function()
+      vim.fn.matchadd("TelescopeParent", "\t\t.*$")
+      vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
+    end)
+  end,
+})
+
+local function filenameFirst(_, path)
+  local tail = vim.fs.basename(path)
+  local parent = vim.fs.dirname(path)
+  if parent == "." then return tail end
+  return string.format("%s\t\t%s", tail, parent)
+end
+
+require("telescope").setup {
+  pickers = {
+    find_files = {
+      path_display = filenameFirst,
+    }
+  }
+}
