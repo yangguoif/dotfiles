5912417d9039974408d41da66f6cb77e409ba06b
Yang Guo <yangguoif@gmail.com>
2025-04-21 11:57:36 +1200

    reorganise

--- macos/nvim/lua/settings/init.lua
+++ macos/nvim/lua/settings/init.lua
@@ -1,1 +1,5 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+require('settings.set')
 require('settings.remap')
--- macos/nvim/lua/settings/remap.lua
+++ macos/nvim/lua/settings/remap.lua
@@ -1,6 +1,27 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
 
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+-- navigate quickfix
 vim.keymap.set('n', '<C-j>', ':cnext<CR>')
 vim.keymap.set('n', '<C-k>', ':cprev<CR>')
+
+-- moving block in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
