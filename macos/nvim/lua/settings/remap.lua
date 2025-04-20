092f23b6dafb65ee94b7d6cef310f50e5498409c
Yang Guo <yangguoif@gmail.com>
2025-04-21 11:10:40 +1200

    keymap for quickfix

--- macos/nvim/lua/settings/remap.lua
+++ macos/nvim/lua/settings/remap.lua
@@ -2,3 +2,5 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set('n', '<C-j>', ':cnext<CR>')
+vim.keymap.set('n', '<C-k>', ':cprev<CR>')
